feff2835be893475222a028aeca0027f8ca6faa6
path-yu <1974675011@qq.com>
2023-02-09 16:49:23 +0800

    add request

---
+++ pages/api/callback.js
@@ -0,0 +1,30 @@
+let client_id = "ba7d6d4a56644b198aa47bb9d88cfc17";
+let client_secret = "a348122d48eb46ae94fb2eee5d8cf2a6";
+import request from "request";
+
+export default function handler(req, res) {
+  let code = req.query.code || null;
+  let redirect_uri = req.query.redirect_uri;
+  var authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    form: {
+      code: code,
+      redirect_uri: redirect_uri,
+      grant_type: "authorization_code",
+    },
+    headers: {
+      Authorization:
+        "Basic " +
+        new Buffer(client_id + ":" + client_secret).toString("base64"),
+    },
+    json: true,
+  };
+
+  request.post(authOptions, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      res.send(body);
+    } else {
+      res.send({ error: "invalid_token", code: 400 });
+    }
+  });
+}
