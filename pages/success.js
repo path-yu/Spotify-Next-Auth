edaf9cf04b26a19219996a64fd17fa9abc6e01f6
path-yu <1974675011@qq.com>
2023-02-11 14:09:48 +0800

    test

--- pages/success.js
+++ pages/success.js
@@ -1,29 +1,27 @@
 import Button, { ButtonProps } from "@mui/material/Button";
-import { useRouter } from "next/router";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-import { styled } from "@mui/material/styles";
 export default function Success() {
   const btn = useRef();
-  const route = useRouter();
+  const [href, setHref] = useState("");
   useEffect(() => {
-    btn.current.click();
-  }, [route.query]);
-  const handleClick = () => {
     let query = new URLSearchParams(location.search);
     let refresh_token = query.get("refresh_token");
     let access_token = query.get("access_token");
     if (refresh_token && access_token) {
-      window.location.href =
+      setHref(
         location.origin +
-        `/center?refresh_token=${refresh_token}&access_token=${access_token}`;
+          `/center?refresh_token=${refresh_token}&access_token=${access_token}`
+      );
     }
-    1;
-  };
+  }, [href]),
+    [];
   return (
     <div>
-      <Button size="large" ref={btn} variant="text" onClick={handleClick}>
-        Back App
+      <Button size="large" ref={btn} variant="text">
+        <a href={href} style={{ textDecoration: "none", color: "#1BC458" }}>
+          Back App
+        </a>
       </Button>
     </div>
   );
